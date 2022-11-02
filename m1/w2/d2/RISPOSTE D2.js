36c94a39405d910cf6f0c9523829f8e5858e4cac
LFantini67 <luca.fantini1195@gmail.com>
2022-11-02 01:36:03 +0100

    second commit

---
+++ m1/w2/d2/RISPOSTE D2.js
@@ -0,0 +1,70 @@
+/* 
+RISPOSTA ESERCIZIO 1
+
+I principali datatype in JavaScript sono:
+
+- Il dato STRINGA - è una sequenza di caratteri. E' un dato alfanumerico che per essere definito necessita dei deliminatori, 
+che possono essere apici o virgolette. E' fondamentale che i deliminatori siano dello stesso tipo, se la stringa è aperta 
+con un apice deve essere chiusa da un apice, idem con le virgolette.
+
+- Il dato NUMERO - è un dato di tipo numerico. Intero o decimale che sia verrà indicato senza distinzione. Il decimale va 
+espresso con il punto anzichè la virgola.
+
+- Il dato BOLEANO - è un dato tipico dei controlli condizionali. Può assumere solo i valori di true o false.
+
+- Il dato NULL - è un tipo di dato rilevabile nelle variabili dichiarate e sta a segnalare la mancata assegnazione di un valore.
+
+- Il dato UNDEFINED - è un tipo di dato rilevabile quando una variabile non ha un valore assegnato oppure non è affatto definita.
+
+*/
+
+/* 
+RISPOSTA ESERCIZIO 2
+ */
+
+var nome = 'Luca';
+
+/* 
+RISPOSTA ESERCIZIO 3
+ */
+
+var x = 12;
+var y = 20;
+function somma(x, y) {
+return x + y;
+}
+console.log(somma(x, y))
+
+/* 
+RISPOSTA ESERCIZIO 4
+ */
+
+var numero = 67;
+
+/* 
+RISPOSTA ESERCIZIO 5
+ */
+
+var name = 'Luca'; {
+const x = 'Fantini';
+}
+console.log(name);
+
+/* 
+RISPOSTA ESERCIZIO 6
+ */
+
+var z = 4;
+function sottrazione(z, x) {
+return z - x;
+}
+console.log(sottrazione(z, x))
+
+/* 
+RISPOSTA ESERCIZIO 7
+*/
+
+var name1 ='john'
+var name2 = 'John'
+
+console.log(name1 == name2)
