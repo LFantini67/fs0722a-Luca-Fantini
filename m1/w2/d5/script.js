2cf32d46d02bab35f59e5ee334aaddf90b2c17c7
LFantini67 <luca.fantini1195@gmail.com>
2022-11-05 01:36:07 +0100

    second commit

--- m1/w2/d5/script.js
+++ m1/w2/d5/script.js
@@ -1,25 +1,29 @@
-function aggiungiCifra(elemento){
+function aggiungiCifra(elemento){   //tramite la funzione aggiungiCifra ho consentito la visualizzazione dei tasti premuti sul display
     
-    console.log(elemento.getAttribute('data-simbolo'));
+    console.log(elemento.getAttribute('data-simbolo')); //tramite getAttribute sono andato a richiamare l'attributo personalizzato
     
-    let cifraCliccata = elemento.getAttribute('data-simbolo');
+    let cifraCliccata = elemento.getAttribute('data-simbolo');  
 
     document.getElementById('display').value += cifraCliccata;
 }
 
-function resettaValore(elemento){
+function resettaValore(elemento){   //tramite la funzione resettaValore ho consentito lo svuotamento del display premendo il relativo tasto
     
     document.getElementById('display').value = ''
 }
 
-function radiceQuadrata(elemento){
+function radiceQuadrata(elemento){ //funzione per il calcolo della radice quadrata
     
     let numeri = document.getElementById('display');
 
     numeri.value = Math.sqrt(numeri.value);
 }
 
-function tot(){
+function calcPercentuale(elemento){ //funzione per il calcolo percentuale (n*n1/100)
+    
+}
+
+function tot(){ // tramite la funzione tot utilizzata mediante eval abbiamo trasformato la stringa in numero ed eeguito l'operazione richiesta
     
     let numeri = document.getElementById('display');
 
